2f4cb575dbc6ba3b0ffb91cdcbe3cd1151221739
Errafai <errafaiachraf1923@gmail.com>
2024-01-17 13:52:49 +0100

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,13 +3,11 @@ import random
 import math
 number = random.randint(-10000, 10000)
 n = number % 10
-m = n
 if number < 0:
     n = n - 10
-    m = -n
-if 0 < m < 6:
+if n != 0 and n <6:
     str = "and is less than 6 and not 0"
-elif m > 5:
+elif n > 5:
     str = "and is greater than 5"
 else:
     str = "and is 0"
