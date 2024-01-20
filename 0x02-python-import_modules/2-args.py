869e9343a37e7d8a8b7e22eeb44080f1c9d7247e
Errafai <errafaiachraf1923@gmail.com>
2024-01-20 17:50:56 +0100

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv as av
+j = len(av)
+if j == 1:
+    print("0 arguments.")
+elif j == 2:
+    print("1 argument:")
+    print("1: {}".format(av[1]))
+else:
+    print("{} arguments:".format(j-1))
+    for i in range(1,j):
+        print("{}: {}".format(i, av[i]))
