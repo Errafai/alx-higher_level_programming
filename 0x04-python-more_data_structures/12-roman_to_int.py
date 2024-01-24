902342801d2fc6d28f28e109aa8522ab175f1e4b
Errafai <errafaiachraf1923@gmail.com>
2024-01-25 00:46:07 +0100

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,11 @@
 def roman_to_int(roman_string):
     if roman_string is None:
         return 0
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     s = 0
     for i in range(len(roman_string)):
-        c = roman[roman_string[i]] * (roman_string[i] in roman)
-        if i < len(roman_string) - 1 and roman[roman_string[i]] < roman[roman_string[i + 1]]:
+        c = r[roman_string[i]] * (roman_string[i] in r)
+        if i < len(roman_string) - 1 and r[roman_string[i]] < r[roman_string[i + 1]]:
             s -= c
         else:
             s += c
