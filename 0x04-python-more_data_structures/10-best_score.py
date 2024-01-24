a1660659873c9c186fb05b331a12d7a3de327ce4
Errafai <errafaiachraf1923@gmail.com>
2024-01-24 16:49:26 +0100

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary == None:
         return None
-    max = list(a_dictionary.item())[0][1]
+    max = list(a_dictionary.item())[0][0]
     for i, j in a_dictionary.items():
-        if a_dictionary[i] >= max:
-            max = j
+        if a_dictionary[i] >= a_dictionary[max]:
+            max = i
     return max
