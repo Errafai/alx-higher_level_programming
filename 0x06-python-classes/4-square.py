34f657b0167f8b27b809173dbf0692ac1cdaf148
Errafai <errafaiachraf1923@gmail.com>
2024-02-14 21:09:26 +0100

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,15 +10,7 @@ class Square:
 
         Args:
             size (int): the size of the square.
-
-        Raises:
-            TypeError: if size is not an integer
-            ValueError: if size is negative
         """
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
         self.__size = size
 
     def area(self):
@@ -37,4 +29,8 @@ class Square:
     @size.setter
     def size(self, size):
         """edit the size"""
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
         size.__size = size
