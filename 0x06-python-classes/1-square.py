88749b8ec8869e1b00a908a01a837188f233e39f
Errafai <errafaiachraf1923@gmail.com>
2024-02-14 13:03:23 +0100

    4-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """the square module"""
 
+
 class Square:
     """square with the attribute size"""
-    def __init__(self, size)
-        """initialize the size attribute"""
-       self.__size = size
+    def __init__(self, size):
+        """the constructor of the class.
 
+        Args:
+            size: the size of the square
+        """
+        self.__size = size
