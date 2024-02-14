013f73bcbe755bc581844e15bc5544b3665f97ae
Errafai <errafaiachraf1923@gmail.com>
2024-02-14 12:31:54 +0100

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,10 +2,17 @@
 """square class module"""
 
 class Square:
+    """the square classs with the attribute size"""
     def __init__(self, size=0):
-
+    """initialize the size of the square
+    Args:
+        size (int): the size of the square
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is negative
+    """
         if not isinstance(size, int):
-            raise ValueError('size must be an integer')
+            raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
