cc6de833d275fdfc62b6fe81bce7050aee491bd1
Errafai <errafaiachraf1923@gmail.com>
2024-02-14 11:23:48 +0100

    3

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,7 +2,7 @@
 """square class module"""
 
 class Square:
-    def __init__(self, size):
+    def __init__(self, size=0):
 
         if not isinstance(size, int):
             raise ValueError('size must be an integer')
