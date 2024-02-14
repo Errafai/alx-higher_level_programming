103a4c247f5c15bc1a938135b45f244ed7c8bb9f
Errafai <errafaiachraf1923@gmail.com>
2024-02-14 12:51:28 +0100

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""square class module."""
+
+
+class Square:
+    """the square classs with the attribute size."""
+
+    def __init__(self, size=0):
+        """initialize the size of the square.
+
+        Args:
+            size (int): the size of the square.
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is negative
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+    def area(self)
+        """calculate the area of the square.
+
+        Return:
+            int: the area of the square
+        """
+        return size ** 2
