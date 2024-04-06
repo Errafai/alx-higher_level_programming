def8aea6120988eb1bcf152b5825add96f9f7b92
Errafai <errafaiachraf1923@gmail.com>
2024-04-06 12:09:57 +0000

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         while my_list[i] and i < x:
-            print(my_list[i], end = "")
+            print(my_list[i], end="")
             i += 1
         print()
-    except:
+    except Exception:
         print()
         pass
     finally:
