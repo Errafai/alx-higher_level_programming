7c9d05f45dccd8d4053c024672afb7cf1c779ec9
Errafai <errafaiachraf1923@gmail.com>
2024-05-23 08:33:31 +0100

    1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,6 +1,4 @@
 -- Create new database even it exists
--- Delete database if already exist
-DROP DATABSE hbtn_0c_0;
 -- Create new database
-CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
