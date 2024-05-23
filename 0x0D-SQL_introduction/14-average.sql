87ee9d06346e5510e6b32b8c9a3dee9587e4393f
Errafai <errafaiachraf1923@gmail.com>
2024-05-23 15:27:15 +0100

    14-average.sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,2 @@
 -- calculate the avg of score in the scond_table
-ALTER TABLE second_table ADD COLUMN IF NOT EXISTS average DECIMAL(10,4);
-UPDATE second_table SET average = (SELECT AVG(score) FROM second_table);
-SELECT average FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
