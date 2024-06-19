6626f656565a1d7eb89c4e7af9db85a20ba48bdf
Errafai <errafaiachraf1923@gmail.com>
2024-06-19 08:34:19 +0100

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const args = process.argv;
 
-if (args.length > 2) {
+if (args.length === 3) {
   console.log('Argument found');
+} else if (args.lenght > 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
