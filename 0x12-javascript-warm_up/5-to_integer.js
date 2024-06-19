03f25f586160afaaa74b95e1f9a84acf47aabbe7
Errafai <errafaiachraf1923@gmail.com>
2024-06-19 11:38:27 +0100

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (typeof Number(process.argv[2]) === 'number') {
-  console.log(Number(process.argv[2]));
+if (Number(process.argv[2])) {
+  console.log(parseInt(process.argv[2]));
 } else {
   console.log('Not a number');
 }
