0e855e3c1e6c120a613706cdb6d7218ce312cff0
Errafai <errafaiachraf1923@gmail.com>
2024-06-19 20:06:40 +0100

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const argv = process.argv;
-if (Number(argv[2])) {
+if (Number(argv[2]) || Number(argv[2]) === 0) {
   for (let i = 0; i < parseInt(argv[2]); i++) {
     let line = '';
     for (let j = 0; j < parseInt(argv[2]); j++) {
-      line += 'x';
+      line += 'X';
     }
     console.log(line);
   }
