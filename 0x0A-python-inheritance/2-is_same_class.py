e568c71b49ff6982b45e2a86cb3b4fb85dd37580
Errafai <errafaiachraf1923@gmail.com>
2024-07-05 11:17:40 +0100

    3-is_kind_of_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,4 +5,4 @@
 def is_same_class(obj, a_class):
     """return false if obj is the same as a_class
     or True if the obj is the same as a_class"""
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and issubclass(obj, a_class)
