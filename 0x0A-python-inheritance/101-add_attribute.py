ff344c8ef4b13853fc2b7090c9df772f8f499c52
Errafai <errafaiachraf1923@gmail.com>
2024-07-06 17:41:06 +0100

    101-add_attribute.py

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""contains the add_attribute"""
+
+def add_attribute(obj, attr, value):
+    """adds an attribue to the obj class
+
+    Args:
+        obj (class): the class to add attribute
+        attr (str): the name of attribute
+        value (object): the value of the attr
+    Raises:
+        TypeError: if we can't add the attr
+    """
+
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    obj.__dict__[attr] = value
