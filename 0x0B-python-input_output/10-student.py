240c4ad99241dbffb8465a4dd5a564a7cb9a3349
Errafai <errafaiachraf1923@gmail.com>
2024-07-08 19:08:59 +0100

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""this module contains the student class"""
+
+
+class Student:
+    """represent the student module"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize the student class
+
+        Args:
+            first_name (str): the first name of the student
+            last_name (str): the last name of the student
+            age (int): the age of the student
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return a dictionary representation of a Student instance"""
+
+        if attrs is None:
+            return self.__dict__
+        else:
+            rep = {}
+            for value in attrs:
+                if hasattr(self, value):
+                    rep[value] = self.__dict__[value]
+            return rep
