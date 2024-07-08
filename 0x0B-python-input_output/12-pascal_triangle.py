4e8ec60db050f912b35a8d23b9782c71658c35c0
Errafai <errafaiachraf1923@gmail.com>
2024-07-08 15:20:34 +0100

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""this module implement the function to construct the
+pascale triangle"""
+
+
+def pascal_triangle(n):
+    """construct the pascale triangle using nested loops"""
+
+    tr = []
+    if n <= 0:
+        return tr
+    tr.append([1])
+    if n == 0:
+        return tr
+    tr.append([1, 1])
+    if n == 1:
+        return tr
+    for i in range(2, n):
+        ll = []
+        ll.append(1)
+        for j in range(1, i):
+            ll.append(tr[i - 1][j-1] + tr[i-1][j])
+        ll.append(1)
+        tr.append(ll)
+    return tr
