bc3da3c02bb752ab3b82885a13800d6ab0f9d481
Errafai <errafaiachraf1923@gmail.com>
2024-07-08 12:15:18 +0100

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""this module uses a bunche of modules to implement the
-function add_item()"""
+"""this module uses a bunche of modules to implement the add_item"""
 
 
 import sys
 """to use the argv and argc arguments"""
 
-jtop = __import__('6-load_from_json_file').load_from_json_file
+load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-ptoj = __import__('5-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
 
-with open(filename, "r") as f:
-    if f.read() == "":
-        jdata = []
-
-
-jdata = jtop(filename)
+try:
+    jdata = load_from_json_file(filename)
+except Exception:
+    jdata = []
 
 for i in sys.argv[1:]:
     jdata.append(i)
 
-ptoj(jdata, filename)
+save_to_json_file(jdata, filename)
