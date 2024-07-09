12abaf78971e00b7b7d9e5f261b9c24cba5178bc
Errafai <errafaiachraf1923@gmail.com>
2024-07-09 11:51:07 +0100

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -19,7 +19,9 @@ def append_after(filename="", search_string="", new_string=""):
         lines = file.readlines()
         i = 0
         for line in lines:
-            if line.find(search_string) != -1:
+            if (search_string == "" or new_string == ""):
+                break
+            if search_string in line:
                 lines.insert(i + 1, new_string)
             i += 1
         file.seek(0)
