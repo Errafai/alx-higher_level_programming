4725b92561e7ebfa095154b43098a7b8629a2919
Errafai <errafaiachraf1923@gmail.com>
2024-08-19 05:54:48 +0100

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""this module contain a function that split strings"""
+
+def text_indentation(text):
+    """this function split the text if he finds these delimeters `.`, `?`
+    and `:`, and replace it with 2 new lines
+
+    Args:
+        text (str): the string to be split it
+
+    Raises:
+        TypeError: if the text is not a string
+    """
+
+    j = 0
+    while j < len(text):
+        if text[j] in ".?:":
+            print(text[j] + "\n")
+            j += 1
+            continue
+        if text[j] == ' ' and text[j - 1] in ".?:":
+            j += 1
+            continue
+        print(text[j], end="")
+        j += 1
