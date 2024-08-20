f971a1c48710e306fd27254d42f9fa15787ebacb
Errafai <errafaiachraf1923@gmail.com>
2024-08-20 12:54:16 +0100

    models/square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""this module contian the square class"""
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """represent the square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize the square class
+
+        Args:
+            size (int): the size of the square
+            x (int): the position of the square in the x axis (spaces)
+            y (int): the position of the square in the y axis (newlines)
+            id (int): the id of the created square
+        """
+        Rectangle.__init__(self, size, size, x, y, id)
+
+    def __str__(self):
+        """retpresent the square class infomations"""
+
+        square = "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
+        return square
