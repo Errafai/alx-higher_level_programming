21e010705e82b02cde7fd2f3ed619fbeaf9847fb
Errafai <errafaiachraf1923@gmail.com>
2024-08-20 10:01:40 +0100

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,3 +96,9 @@ class Rectangle(Base):
 
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """represent the rectange informations"""
+        rectangle = ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                    self.__y, self.__width, self.__height))
+        return rectangle
