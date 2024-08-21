87233793597a9dc1103ffa7c36fa0a75c68eef62
Errafai <errafaiachraf1923@gmail.com>
2024-08-21 08:50:30 +0100

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -65,6 +65,6 @@ class Square(Rectangle):
         return {
                 "id": self.id,
                 "size": self.size,
-                "x": self.__x,
-                "y": self.__y
+                "x": self.x,
+                "y": self.y
             }
