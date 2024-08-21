524a3abcb4a8847908df4cefaf07768953fc9c32
Errafai <errafaiachraf1923@gmail.com>
2024-08-21 09:26:01 +0100

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -58,7 +58,7 @@ class Base:
         """create new intance of the cls type and assigne
         attribute that already exist in the dictionary"""
 
-        dummy = cls()
+        dummy = cls(3)
 
         dummy.update(**dictionary)
 
