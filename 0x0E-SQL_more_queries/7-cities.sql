85b92b084100af7246582abbfa6d9fe5aa904c09
Errafai <errafaiachraf1923@gmail.com>
2024-09-04 07:39:23 +0100

    7-cities.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- create the table cities in the databse hbtn_od_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+		id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+		state_id INT NOT NULL,
+		FOREIGN KEY(state_id) REFERENCES states(id),
+		name VARCHAR(256)
+		);
