0f64d2aeb187d53656ec9e707b5c10f718e015d7
Errafai <errafaiachraf1923@gmail.com>
2024-09-06 21:29:38 +0100

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,9 @@
 -- list all tv shows with thier genresa
 SELECT tv_genres.name as genre, number.number_of_shows
 	FROM tv_genres INNER JOIN (
-		SELECT genre_id, COUNT(show_id)
-		AS number_of_shows
+		SELECT genre_id, COUNT(show_id) AS number_of_shows
 		FROM tv_show_genres
 		GROUP BY genre_id
 	) AS number
-	on number.genre_id = tv_genres.id
+	ON number.genre_id = tv_genres.id
 	ORDER BY number_of_shows DESC;
