a625880c60ea4e8cc904156d039a43710f4ae8fd
Errafai <errafaiachraf1923@gmail.com>
2024-09-07 13:31:38 +0100

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,8 @@
 -- displays the genres of Dexter tv show
 SELECT DISTINCT name
 	FROM tv_genres JOIN tv_show_genres
-	WHERE (SELECT id FROM tv_shows WHERE title = "Dexter") = tv_show_genres.show_id AND tv_show_genres.genre_id = tv_genres.id
+	WHERE (SELECT id
+		FROM tv_shows
+		WHERE title = "Dexter"
+	) = tv_show_genres.show_id AND tv_show_genres.genre_id = tv_genres.id
 	ORDER BY name ASC;
