54669a5e206ea05a29f1a997cf245838bce149ae
Errafai <errafaiachraf1923@gmail.com>
2024-09-08 12:06:09 +0100

    101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,7 @@
 -- displays all the tv shows that don't have a commed genre
 SELECT DISTINCT title
 	FROM tv_shows
-	LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-	LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id AND tv_genres.name  = "Comedy"
+	LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id AND tv_show_genres.genre_id = 5
+	LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id AND tv_genres.name = "Comedy"
 	WHERE tv_genres.id IS NULL
 	ORDER BY title ASC;
