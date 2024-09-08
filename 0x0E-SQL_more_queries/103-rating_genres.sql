4b040a1bb9fd23641af3e386389583f37c322d62
Errafai <errafaiachraf1923@gmail.com>
2024-09-08 12:36:46 +0100

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,4 +4,4 @@ SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 	INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 	INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 	GROUP BY tv_genres.name
-	ORDER BYrating DESC;
+	ORDER BY rating DESC;
