5b4ca86531bd4a5501650af2e49eff2b6541f046
Errafai <errafaiachraf1923@gmail.com>
2024-09-08 11:16:00 +0100

    100-not_my_genres.sql

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,10 @@
+-- show all genres that are not related to 'Dexter' tv show
+ SELECT name
+	FROM tv_genres LEFT JOIN (
+		SELECT title, genre_id
+		FROM tv_shows INNER JOIN tv_show_genres
+		ON tv_shows.id = tv_show_genres.show_id
+		WHERE title = 'Dexter'
+	) AS shows
+	ON tv_genres.id = shows.genre_id
+	WHERE shows.genre_id IS NULL;
