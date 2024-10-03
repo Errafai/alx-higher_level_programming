66eefbaccbd685b5f01d08cbc2239e21b512f42d
Errafai <errafaiachraf1923@gmail.com>
2024-10-03 17:41:54 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,11 +4,15 @@ const av = process.argv;
 if (av.length <= 3) {
   console.log('0');
 } else {
-  let min = Number(av[2]);
+  let max1 = Number(av[2]);
+  let max2 = Number(av[2]);
   for (const item of av.slice(3)) {
-    if (min > Number(item)) {
-      min = Number(item);
+    if (max1 < Number(item)) {
+      max1 = Number(item);
+    }
+    if (max2 < Number(item) && Number(item) !== max1) {
+      max2 = Number(item);
     }
   }
-  console.log(min);
+  console.log(max2);
 }
