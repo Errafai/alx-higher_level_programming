1e83ae6334b73880bbc9be4695a27a6fb802171f
Errafai <errafaiachraf1923@gmail.com>
2024-10-25 14:34:19 +0100

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""this module connect the hbtn_0e_0_usa module and select all data
+in the satates table """
+
+import MySQLdb
+import sys
+
+ar = sys.argv
+
+connector = MySQLdb.connect(host="localhost", user=ar[1], passwd=ar[2],
+                            port=3306, db=ar[3]
+                            )
+cursor = connector.cursor()
+
+cursor.execute("SELECT * FROM states")
+
+states = cursor.fetchall()
+
+for state in states:
+    print(state)
