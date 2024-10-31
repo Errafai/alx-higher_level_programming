5b3d32e9950465e30dc4b9c87128337f4f1ecf97
Errafai <errafaiachraf1923@gmail.com>
2024-10-31 11:06:59 +0100

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
-"""
+"""Start link class to table in database"""
+
+
 import sys
 from model_state import Base, State
 
@@ -15,7 +16,6 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
-
