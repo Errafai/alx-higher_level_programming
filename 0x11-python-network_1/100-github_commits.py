50cdeb1242264e5489bc18a310d824bd93f7eb43
Errafai <errafaiachraf1923@gmail.com>
2025-06-23 22:13:26 +0100

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/"
     url +=  sys.argv[1] + "/" + sys.argv[2] + "/commits"
    
+    res = req.get(url)
     for i in range(10):
-        res = req.get(url)
-        data = res.json()[0]
+        data = res.json()[i]
         sha = data["sha"]
         name = data["commit"]["author"]["name"]
         print("{}: {}".format(sha, name))
