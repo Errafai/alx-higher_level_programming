04647abf816df53be8a4124bc08fd68f2efe9946
Errafai <errafaiachraf1923@gmail.com>
2025-06-23 22:00:24 +0100

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""listing the last 10 commit of a repo and its owner"""
+
+
+if __name__ == "__main__":
+    import requests as req
+    import sys
+
+    url = "https://api.github.com/repos/" + sys.argv[1] + "/" + sys.argv[2] + "/commits"
+    
+    for i in range(10):
+        res = req.get(url)
+        data = res.json()[0]
+        sha = data["sha"]
+        name = data["commit"]["author"]["name"]
+        print("{}: {}".format(sha, name))
